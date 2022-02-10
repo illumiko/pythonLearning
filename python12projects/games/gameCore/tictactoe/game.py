13a2754bed536dbbf2feb03947213e5d84cbae51
illumiko <eyanat.rayannjr@gmail.com>
2022-02-11 02:58:39 +0600

    added most of the imp game functions just adding player functions and win condition left

--- python12projects/games/gameCore/tictactoe/game.py
+++ python12projects/games/gameCore/tictactoe/game.py
@@ -21,6 +21,17 @@ class TicTacToe():
     @staticmethod    
     def printBoardNums():
         numberBoard = [[str(i) for i in range(j*3, j(1+1)*3)] for j in range(3)]
+        for x in numberBoard:
+            print(" | "+" | ".join(x)+" | ")
+
+    def emptySpaces(self):
+        return "_" in self.board
+
+    def emptySpacesNum(self):
+        return self.board.count("_")
+
+    def availableMoves(self):
+        return "_" in self.board
 
     """""
     @staticmethod
@@ -29,7 +40,38 @@ class TicTacToe():
         print(numberBoard)
     """""
 
+def play(game,aPlayer,bPlayer, printGame=True):
+    if printGame:
+        game.printBoardNums()
+    letter = 'X'
+
+    while game.emptySpaces():
+        if letter == "O":
+            square = bPlayer.get_move(game)
+        else:
+            square = aPlayer.get_move(game)
+
+        if game.makeMove(square, letter):
+            if printGame:
+                print(letter + 'makes a move to sqaure', square)
+                game.printBoard()
+                print('')
+
+            if game.currentWinner:
+                if printGame:
+                    print(letter + 'wins!')
+                return letter # ends the loop and exits
+            letter = 0 if letter == "X" else "X"
+        time.sleep(.8)
+
+    if printGame:
+        print("its a tie")
+
+
+
+
 x = TicTacToe()
+x.printBoardNums()
 
         
 
