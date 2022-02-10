68b44df2d5bd26c1785a60dcf03fd31fa35b899a
illumiko <eyanat.rayannjr@gmail.com>
2022-02-10 21:01:27 +0600

    added player.py

---
+++ python12projects/games/gameCore/tictactoe/player.py
@@ -0,0 +1,20 @@
+import math
+import random
+
+class Player:
+    def __init__(self, letter) -> None:
+        # letter is X or 0
+        self.letter = letter
+    def get_move(self, game):
+        pass
+
+class RandomComputerPlayer(Player):
+    def __init__(self, letter) -> None:
+        super().__init__(letter)
+    def get_move(self,game):
+        pass
+class HumanPlayer(Player):
+    def __init__(self, letter) -> None:
+        super().__init__(letter)
+    def get_move(self,game):
+        pass
