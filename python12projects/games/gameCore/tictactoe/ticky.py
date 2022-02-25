596983b754cc8fe4a402e1dbec676fad08162b14
illumiko <eyanat.rayannjr@gmail.com>
2022-02-25 15:54:13 +0600

    added board gen w/o nums and board formatter

--- python12projects/games/gameCore/tictactoe/ticky.py
+++ python12projects/games/gameCore/tictactoe/ticky.py
@@ -1,15 +1,25 @@
-class Player:
-    def __init__(self, name):
-        self._name = name
-
-    @property
-    def name(self):
-        return self._name
-
-        
-        
 class Game:
     def __init__(self):
-        pass
+        self.gameBoard = self.makeBoard(False)
+
+    @staticmethod
+    def makeBoard(num=False):
+        if num:
+            return [str(i) for i in range(9) ]
+        else:
+            return ["_" for i in range(9) ]
+
+    @staticmethod
+    def printBoard(board):
+        segment = [board[i*3:(i+1)*3] for i in range(3)]
+        for i in range(3):
+            print(" | " +" | ".join(segment[i])+" | ")
+            
+
+
+
+test = Game()
+print(test.printBoard(test.makeBoard(True)))
+
         
 
--- python12projects/games/gameCore/tictactoe/tickyPlayer.py
+++ python12projects/games/gameCore/tictactoe/tickyPlayer.py
@@ -2,24 +2,16 @@ class Player:
     def __init__(self, name,letter):
         self._name = name
         self.points = 0
-        self.withdrawed = False
         self.letter = letter
 
     @property
     def name(self):
         return self._name
 
-"""""
-    def withdraw(self):
-        pass
-    
-    def makeMove(self,gameBoard):
-        usrInput = input("Enter a number from(0-8)")
-        gameBoard[usrInput] = self.letter
-"""""
 class HumanPlayer(Player):
     def __init__(self, name, letter):
         super().__init__(name, letter)
+        self.withdrawed = False
 
     def withdraw(self):
         self.withdrawed = True
@@ -32,4 +24,10 @@ class HumanPlayer(Player):
 class ComputerPlayer(Player):
     def __init__(self, name, letter):
         super().__init__(name, letter)
+
+    def makeMove(self, gameBoard):
+        import random
+        index = random.choice([i for i in range(9)])
+        gameBoard[index] = self.letter
+
 
