c8db70ef14f6e6eff2785b9ccb810033ba1d59a9
illumiko <eyanat.rayannjr@gmail.com>
2022-02-25 12:55:54 +0600

    completed tictactoe

--- python12projects/games/gameCore/tictactoe/game.py
+++ python12projects/games/gameCore/tictactoe/game.py
@@ -1,5 +1,6 @@
 import math
 import time
+from player import HumanPlayer,RandomComputerPlayer
 
 class TicTacToe():
     def __init__(self) -> None:
@@ -20,18 +21,49 @@ class TicTacToe():
             print(" | "+" | ".join(row) +" | ")
     @staticmethod    
     def printBoardNums():
-        numberBoard = [[str(i) for i in range(j*3, j(1+1)*3)] for j in range(3)]
+        numberBoard = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
         for x in numberBoard:
             print(" | "+" | ".join(x)+" | ")
 
     def emptySpaces(self):
+        # retruns boolen
         return "_" in self.board
 
     def emptySpacesNum(self):
         return self.board.count("_")
 
     def availableMoves(self):
-        return "_" in self.board
+        # return "_" in self.board
+        return [i for i,spot in enumerate(self.board) if spot == "_"]
+
+    def makeMove(self,square,letter):
+        if self.board[square] == "_":
+            self.board[square] = letter
+            if self.winCondition(square,letter):
+                self.currentWinner = letter
+            return True
+        return False
+
+    def winCondition(self,square,letter):
+        horizontalWin = [self.board[(square//3)*3+x] for x in range(3)]
+        verticleWin = [self.board[square%3+x*3] for x in range(3)]
+        if all(square == letter for square in horizontalWin):
+            return True
+        if all(square == letter for square in verticleWin):
+            return True
+        if square % 2 == 0:
+            diagonal1 = [self.board[i] for i in [0, 4, 8]]
+            # print('diag1', diagonal1)
+            if all([s == letter for s in diagonal1]):
+                return True
+            diagonal2 = [self.board[i] for i in [2, 4, 6]]
+            # print('diag2', diagonal2)
+            if all([s == letter for s in diagonal2]):
+                return True
+        return False
+            
+
+
 
     """""
     @staticmethod
@@ -61,7 +93,7 @@ def play(game,aPlayer,bPlayer, printGame=True):
                 if printGame:
                     print(letter + 'wins!')
                 return letter # ends the loop and exits
-            letter = 0 if letter == "X" else "X"
+            letter = 'O' if letter == "X" else "X"
         time.sleep(.8)
 
     if printGame:
@@ -69,10 +101,10 @@ def play(game,aPlayer,bPlayer, printGame=True):
 
 
 
-
-x = TicTacToe()
-x.printBoardNums()
-
+if __name__ == '__main__':
+    x_player = RandomComputerPlayer('X')
+    o_player = HumanPlayer('O')
+    t = TicTacToe()
+    play(t, x_player, o_player, printGame=True)
         
-
 
--- python12projects/games/gameCore/tictactoe/player.py
+++ python12projects/games/gameCore/tictactoe/player.py
@@ -11,11 +11,26 @@ class Player:
 class RandomComputerPlayer(Player):
     def __init__(self, letter) -> None:
         super().__init__(letter)
+
     def get_move(self,game):
-        pass
+        square = random.choice(game.availableMoves())
+        return square
 
 class HumanPlayer(Player):
     def __init__(self, letter) -> None:
         super().__init__(letter)
     def get_move(self,game):
-        pass
+        validSpaces = False
+        val = None
+        while not validSpaces:
+            square = input(self.letter + "'s turn. Input(0-8)")
+            try:
+                val = int(square)
+                if val not in game.availableMoves():
+                    raise ValueError
+                validSpaces = True
+            except ValueError:
+                print("Invalid move, Try again")
+        return val
+
+
